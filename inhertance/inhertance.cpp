74af2acd186a91c3e12b54adcc6b74fe4a5ecb82
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-20 19:18:13 +0700

    masukkan constuctor

--- inhertance/inhertance.cpp
+++ inhertance/inhertance.cpp
@@ -24,4 +24,8 @@ public:
 class pelajar :public orang {
 public:
 	string sekolah;
+	pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+		cout << "pelajar dibuat\n" <<endl;
+
+	}
 };
