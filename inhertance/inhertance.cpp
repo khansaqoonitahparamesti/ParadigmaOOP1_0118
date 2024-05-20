38f14db09ed10f477611a5a73493c480dd1963a8
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-05-20 19:09:56 +0700

    masukkan class

---
+++ inhertance/inhertance.cpp
@@ -0,0 +1,10 @@
+// inhertance.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+
+class orang{
+
+};
